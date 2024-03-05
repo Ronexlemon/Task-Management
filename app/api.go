b6d4fc69eea886140458230538490a66def46961
RonexLemon <ronexondimu@gmail.com>
2024-03-05 05:42:47 +0300

    updating task

--- app/api.go
+++ app/api.go
@@ -29,3 +29,28 @@ func newTaskHandler(c *gin.Context){
 func GetTaskHandler(c *gin.Context){
 	c.JSON(http.StatusOK,model.Tasks)
 }
+
+
+//updating task
+
+func UpdateTaskHandler(c *gin.Context){
+	id:= c.Param("id")
+	var task model.Task
+
+	if err:= c.ShouldBindJSON(&task); err !=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+	index := -1
+	for i:=0;i < len(model.Tasks);i++{
+		if model.Tasks[i].ID == id{
+			index =i
+		}
+	}
+	if index == -1{
+		c.JSON(http.StatusNotFound,gin.H{"error":"Task not found"})
+		return
+	}
+	model.Tasks[index] = task
+	c.JSON(http.StatusOK,task)
+}
--- model/Task.go
+++ model/Task.go
@@ -14,8 +14,8 @@ type Task struct{
 
 var Tasks = []Task{
 	{ID:"1",Title: "Code the codes",Priority: strconv.Itoa(3),Status: "in Progress",Assignee: "john doe"},
-	{ID:"1",Title: "Get the bag",Priority: strconv.Itoa(1),Status: "in Progress",Assignee: "lemonr lemonr"},
-	{ID:"1",Title: "All the goodies",Priority: strconv.Itoa(2),Status: "To do",Assignee: "john heck"},
-	{ID:"1",Title: "Buy new Machine",Priority: strconv.Itoa(4),Status: "in Progress",Assignee: "Work station"},
-	{ID:"1",Title: "Gain it all",Priority: strconv.Itoa(5),Status: "in Progress",Assignee: "john doe"},
+	{ID:"2",Title: "Get the bag",Priority: strconv.Itoa(1),Status: "in Progress",Assignee: "lemonr lemonr"},
+	{ID:"3",Title: "All the goodies",Priority: strconv.Itoa(2),Status: "To do",Assignee: "john heck"},
+	{ID:"4",Title: "Buy new Machine",Priority: strconv.Itoa(4),Status: "in Progress",Assignee: "Work station"},
+	{ID:"5",Title: "Gain it all",Priority: strconv.Itoa(5),Status: "in Progress",Assignee: "john doe"},
 }
